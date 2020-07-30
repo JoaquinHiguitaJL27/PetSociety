65218e41b79be4996a87dead051f7560a0d4659a
JoaquinHiguitaJL27 <joaquin050218@gmail.com>
2020-07-30 00:24:57 -0500

    Se cambió el proyecto en react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>PetSociety</h1>
+      <h1>Tabla de integrantes</h1>
+      <div>
+      <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>TI</th>
+              <th>Número</th>
+              <th>IE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Joaquín Higuita Lopez</td>
+              <td>joaquin050218@gmail.com</td>
+              <td>1015066347</td>
+              <td>3206270582</td>
+              <td>Javiera Londoño</td>
+            </tr>
+            <tr>
+              <td>David Castañeda Quintero</td>
+              <td>davidcastanedaquintero@gmail.com</td>
+              <td>1025641452</td>
+              <td>3052583246</td>
+              <td>Javiera Londoño</td>
+            </tr>
+            <tr>
+              <td>Sebastián Parra Lopera</td>
+              <td>sebastianparralopera351@gmail.com</td>
+              <td>1011390733</td>
+              <td>3104109270</td>
+              <td>Javiera Londoño</td>
+            </tr>  
+          </tbody>
+        </table>
+        </div>
     </div>
   );
 }
